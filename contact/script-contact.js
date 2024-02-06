b3784974f179d34eeca78d79b1dc959d8c769ecf
mygoshhh123 <zbseow@gmail.com>
2024-02-06 17:44:19 +0800

    update contact

--- contact/script-contact.js
+++ contact/script-contact.js
@@ -1,17 +1,21 @@
+// send data from the contact form to restdb
 document.addEventListener("DOMContentLoaded", function () {
     const APIKEY = "65be42a8459ebb29dd283fad"; // API KEY
 
-    document.getElementById("add-update-msg").style.display = "none";
+    // Hide the update successful message
+    document.getElementById("update-msg").style.display = "none";
 
     document.getElementById("contact-submit").addEventListener("click", function (e) {
         e.preventDefault();
 
+        // validate the form data
         if (validateForm()) {
+            // get the form inputs
             let contactName = document.getElementById("contact-name").value;
             let contactEmail = document.getElementById("contact-email").value;
             let contactNumber = document.getElementById("contact-number").value;
             let contactSubject = document.getElementById("contact-subject").value;
-            let contactMessage = document.getElementById("contact-msg").value;
+            let contactMessage = document.getElementById("contact-message").value;
 
             let jsondata = {
                 "Name": contactName,
@@ -30,6 +34,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 },
                 body: JSON.stringify(jsondata),
                 beforeSend: function () {
+                    // Disable our button or show loading bar
                     document.getElementById("contact-submit").disabled = true;
                 }
             }
@@ -39,34 +44,39 @@ document.addEventListener("DOMContentLoaded", function () {
                 .then(data => {
                     console.log(data);
                     document.getElementById("contact-submit").disabled = false;
-                    document.getElementById("add-update-msg").style.display = "block";
+                    // show update successful message
+                    document.getElementById("update-msg").style.display = "block";
                     setTimeout(function () {
-                        document.getElementById("add-update-msg").style.display = "none";
+                        document.getElementById("update-msg").style.display = "none";
                     }, 3000);
                     getContacts();
-                    document.getElementById("add-contact-form").reset(); // Reset the form
+                    document.getElementById("contact-form").reset(); // Reset the form
                 });
         } 
     });
 
+    // Function to validate form input
     function validateForm() {
         var name = document.getElementById('contact-name').value;
         var email = document.getElementById('contact-email').value;
         var number = document.getElementById('contact-number').value;
         var subject = document.getElementById('contact-subject').value;
-        var message = document.getElementById('contact-msg').value;
+        var message = document.getElementById('contact-message').value;
 
+        // Check if any of the fields are empty
         if (name === '' || email === '' || number === '' || subject === '' || message === '') {
             alert('Please fill in all fields');
             return false;
         }
 
+        // Email Validation
         var validEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!validEmail.test(email)) {
             alert('Please enter a valid email address.');
             return false;
         }
 
+        // Phone Number Validation
         var validPhone = /^[89]\d{7}$/;
         if (!validPhone.test(number)) {
             alert('Please enter a valid phone number with 8 digits, starting with either number "9" or "8".');
@@ -88,28 +98,26 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
-
+// Loading Animation
 document.addEventListener('DOMContentLoaded', function() {
-    // Select the preloading element
-    var preloading = document.getElementById('pre-loading-animation');
-  
-    // Toggle the 'active' class to show or hide the loading animation
-    function toggleLoadingAnimation() {
-        preloading.classList.toggle('active');
+    var loadings = document.getElementsByClassName('loading');
+
+    // use a For loop to loop through each element
+    for (var i = 0; i < loadings.length; i++) {
+        var preloading = loadings[i];
+        preloading.classList.toggle('active'); // to show or hide the animation
     }
-  
-    // Call toggleLoadingAnimation function to show the loading animation
-    toggleLoadingAnimation();
-  
-    // Example: Hide the loading animation after 3 seconds (adjust as needed)
+
     setTimeout(function() {
-        toggleLoadingAnimation();
-    }, 3000);
-});
+        for (var i = 0; i < loadings.length; i++) {
+            loadings[i].classList.toggle('active');
+        }
+    }, 3000); // Show content after 3 seconds
+  });
 
 // Hamburger Menu (for smaller view size)
 document.addEventListener("DOMContentLoaded", function() {
-    const menuIcon = document.getElementById("menu");
+    const menuIcon = document.getElementById("hamburger-menu");
     const navbar = document.querySelector("header .navbar");
   
     // Toggle navbar visibility when menu icon is clicked
